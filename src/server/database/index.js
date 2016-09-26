40ebc8d3757588faaf115e133b107e79e650c7b0
Emir Marques <emirdeliz@gmail.com>
2016-09-26 14:05:16 -0300

    Removido script posinstall

--- src/server/database/index.js
+++ src/server/database/index.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import Professional from './professional';
 
-mongoose.connect('mongodb://localhost/app-bico');
+mongoose.connect('mongodb://heroku_cw2tltpr:j2fffr3d5o9so9vco106n4e65@ds041486.mlab.com:41486/heroku_cw2tltpr');
 
 export { Professional };
